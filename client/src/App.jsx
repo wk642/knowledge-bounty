704385cb6d786eead9c35fcd512b492621d9b406
Winnie Kelley <w642kelley@gmail.com>
2025-04-01 10:12:23 -0500

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-red-300 text-center"> Knowledge Bounty </h1>
+      <p> Ready to gamify blogging?</p>
+    </>
+  )
+}
+
+export default App
