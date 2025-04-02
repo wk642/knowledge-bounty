66149663dba14a34e45718c95783e7142bb6b875
Winnie Kelley <w642kelley@gmail.com>
2025-04-02 16:11:30 -0500

    server, gets all the posts

--- client/App.jsx
+++ client/App.jsx
@@ -1,20 +1,11 @@
-// function App() {
-//   return (
-//     <>
-//       <h1 className="text-red-300 text-center"> Knowledge Bounty </h1>
-//       <p> Ready to gamify blogging?</p>
-//     </>
-//   )
-// }
-
-// export default App
-
-import * as React from 'react';
-
-const title = 'Hello React';
+import React from 'react';
 
 function App() {
-  return <div>{title}</div>;
+  return(
+    <>
+      <h1>Tornado</h1>
+    </>
+  )
 }
 
 export default App;
