829e144822e8fec13b4d6012c6400df12f516577
Eric <eric@everylittlething.net>
2025-04-03 17:56:06 -0700

    allow prettier to fix line endings.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 export const presets = [
-  "@babel/preset-env",
-  ["@babel/preset-react", { runtime: "automatic" }],
+    "@babel/preset-env",
+    ["@babel/preset-react", { runtime: "automatic" }],
 ];
